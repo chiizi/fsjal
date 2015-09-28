e2caa98d04027668015b955122b27edc8367eb82
Hans Felde <m33pfish@gmail.com>
2015-09-28 17:01:02 -0500

    Add immutable tuples

--- fsjal.js
+++ fsjal.js
@@ -1,1 +1,23 @@
-
+var Tuple = function(a, b) {
+  Object.defineProperties(this, {
+    car: {
+      value: function() {
+        return a;
+      },
+      writable: false
+    },
+    cdr: {
+      value: function() {
+        return b;
+      },
+      writable: false
+    },
+    toArray: {
+      value: function() {
+        return [a, b];
+      },
+      writable: false
+    }
+  });
+  return this;
+};
