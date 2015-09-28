dad99b1eee9fb8beafb6ff3e4c35765b026f39ff
Hans Felde <m33pfish@gmail.com>
2015-09-28 17:14:45 -0500

    Add recursive tuple list maker

--- fsjal.js
+++ fsjal.js
@@ -21,3 +21,15 @@ var Tuple = function(a, b) {
   });
   return this;
 };
+var list = function(array) {
+  if (array.length > 1) {
+    var b = array.pop();
+    var a = array.pop();
+    array.push(new Tuple(a, b))
+    return list(array);
+  } else if (array.length == 1) {
+    return array[1];
+  } else {
+    return new Tuple(null, null);
+  }
+};
